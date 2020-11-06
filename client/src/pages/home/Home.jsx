724892c86b9d4a0083eb882c764aaeade2ea066f
Garrett <gparre1@students.towson.edu>
2020-11-06 01:12:22 -0500

    added backend

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,32 +1,47 @@
-import React, {useEffect, useState} from 'react';
+import { useState } from "react";
+import Axios from "axios";
+
+function Home() {
+  const [ID, setID] = useState("");
+  const [Fname, setFname] = useState("");
+  const [Lname, setLname] = useState("");
+  const [DOB, setDOB] = useState("");
+  const [Date, setDate] = useState("");
+  const [Time, setTime] = useState("");
+  const [Status, setStatus] = useState("");
 
 
-function Home(){
-    // const [patients, setPatients] = useState([]);
+  const [data, setData] = useState([]);
 
-    // useEffect(() => {
-    //     fetch('/api/patients')
-    //         .then(res => res.json())
-    //         .then(patients => setPatients(patients));
-    // });
+  const getData = () => {
+    Axios.get("http://localhost:3001/api/patients").then((response) => {
+      setData(response.data);
+    });
+  };
 
-    
+  return (
+    <div className="App">
 
-    return (
-        
-        <div>
-            <h1>Welcome!</h1>
-            <h2>Schedule you appointment today!</h2>
-            <button>Schedule Now!</button>
-            <p>Are you an employee? Click here to sing in.</p>
+      <div className="employees">
+        <button onClick={getData}>Show Data</button>
 
-            {/* <ul>
-                {patients.map(patient => 
-                    <li key={patient.id}>{patient.name} {patient.age}</li>
-                )}
-            </ul> */}
-        </div>
-    );
+        {data.map((val, key) => {
+          return (
+            <div className="employee">
+              <h3>ID: {val.ID}</h3>
+              <h3>Fname: {val.Fname}</h3>
+              <h3>Lname: {val.Lname}</h3>
+              <h3>DOB: {val.DOB}</h3>
+              <h3>Date: {val.Date}</h3>
+              <h3>Time: {val.Time}</h3>
+              <h3>Status: {val.Status}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
+
