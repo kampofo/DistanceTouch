2070ece7a6d9bc72ef4c4e9432f4a340d9af1033
Garrett <gparre1@students.towson.edu>
2020-11-29 22:01:40 -0500

    Garretts final changes.

--- client/src/pages/checkIn/CheckIn.jsx
+++ client/src/pages/checkIn/CheckIn.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
+import Axios from "axios";
 
 
 
-function CheckIn(){
-return (
+function CheckIn() {
+    return (
         <div>
             <h1>Create Your Reservation.</h1>
-            <form action="" method="get">
+            <form action="" method="Post">
 
                 <div>
                     <label htmlFor="first-name">First Name:</label>
-                    <input type="text" id="first-name" name="first-name" placeholder="John"/>
+                    <input type="text" id="first-name" name="first-name" placeholder="John" />
                 </div>
 
                 <div>
-                    <label htmlFor="Last-name">Last Name:</label>
-                    <input type="text" id="Last-name" name="Last-name" placeholder="Doe"/>
+                    <label htmlFor="last-name">Last Name:</label>
+                    <input type="text" id="last-name" name="last-name" placeholder="Doe" />
                 </div>
-                
+
                 <div>
-                    <label for="appointment-time">Select your appointment date and time:</label>
-                    <input type="datetime-local" id="appointment-time" name="appointment-time" />
+                    <label htmlFor="birthday">Date of Birth:</label>
+                    <input type="date" id="birthday" name="birthday" placeholder="Doe" />
                 </div>
+
+                <div>
+                    <label htmlFor="appointment-date">Select your appointment date:</label>
+                    <input type="date" id="appointment-date" name="appointment-date" />
+                </div>
+
+                <div>
+                    <label htmlFor="appointment-time">Select your appointment time:</label>
+                    <input type="time" id="appointment-time" name="appointment-time"
+                        min="09:00" max="18:00"></input>
+                </div>
+
 
                 <button>Create Reservation</button>
             </form>
